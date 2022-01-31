35d40a25a61cbe5b886877a1dd9538aa618e303f
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-31 13:27:18 +0100

    Add header component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import planet from '../images/planet.png';
 
-const Navbar = () => <div />;
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/Missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/MyProfile',
+      text: 'MyProfile',
+    },
+  ];
+
+  return (
+    <header className="header">
+      <nav>
+        <img src={planet} alt="planet logo" />
+        <h1>Space Travellers Hub</h1>
+        <ul>
+          {links.map((item) => (
+            <li key={item.id}>
+              <NavLink to={item.path} className="navlink">
+                {item.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
