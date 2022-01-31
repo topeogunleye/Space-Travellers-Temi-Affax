20a8f84ae3bc6c18a05cd679be48f3a587fc648b
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-31 17:41:36 +0100

    Add Missions reducer

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const DISPLAY_MISSIONS = 'DISPLAY_MISSIONS';
+
+const initialState = {
+  missions: [],
+};
+
+export const displayMissions = (payload) => ({
+  type: DISPLAY_MISSIONS,
+  payload,
+});
+
+// missions.js
+export const fetchMissions = () => (dispatch) => {
+  axios
+    .get('https://api.spacexdata.com/v3/missions')
+    .then((response) => {
+      const missions = response.data.slice(0,5);
+      dispatch(displayMissions(missions));
+    })
+    .catch(() => {});
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_MISSIONS:
+      return {
+        missions: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
