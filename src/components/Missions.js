f6c164ac30a1e409d3a9bf7a9307ce3cddf0402f
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-31 14:40:17 +0100

    Add missions  component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Missions = () => <div />;
+const Missions = () => (
+  <div className="mission-container">
+    <div className="mission">
+      <h2>Mission</h2>
+    </div>
+    <div className="description">
+      Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Aut itaque voluptatibus aperiam voluptatem doloremque,
+      quam eveniet est aliquam rerum animi?
+    </div>
+    <div className="status">
+      <button type="button"> not a member</button>
+      <button type="button"> join  mission</button>
+    </div>
+  </div>
+);
 
 export default Missions;
