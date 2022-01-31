61ae0f2e4e9772e8ca051809c315eee6e5566434
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-31 13:59:06 +0100

    Add Profile Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import MyProfile from './components/MyProfile';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/MyProfile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Profile = () => <div />;
+const MyProfile = () => (
+  <div>
+    <div className="my-profile">
+      <div className="missions"><h2>My Missions</h2></div>
+      <div className="rockets"><h2>My Rockets</h2></div>
+    </div>
+  </div>
+);
 
-export default Profile;
+export default MyProfile;
