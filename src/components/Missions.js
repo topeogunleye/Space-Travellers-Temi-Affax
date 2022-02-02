51e24200c01eb97b093f710daf7ea0952c611c53
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-02 17:49:14 +0100

    Add Styling For Missions Div

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -8,17 +8,21 @@ const Missions = () => {
   const missions = useSelector((state) => state.missionsReducer.missions);
 
   const missionsDiv = missions.map((mission) => (
-    <tr key={mission.mission_id}>
-      <td>{mission.mission_name}</td>
-      <td>{mission.description}</td>
+    <tr
+      key={mission.mission_id}
+      className="odd:bg-gray-200 divide-x divide-gray-300"
+    >
+      <td className="flex align-top pl-1">{mission.mission_name}</td>
+      <td className="px-3 py-2">{mission.description}</td>
       <td>
         {mission.reserved && (
-          <div className="status">
-            <button type="button" className="badge">
+          <div className="status w-64 flex justify-around text-xs">
+            <span className="badge bg-sky-500 text-white rounded-sm px-1 py-0.5 uppercase grid place-items-center">
               Active Member
-            </button>
+            </span>
             <button
               type="button"
+              className="text-sm text-red-600 border-red-400 rounded-sm border-2 px-1 py-0.5"
               onClick={() => dispatch(leaveMission(mission.mission_id))}
             >
               {' '}
@@ -27,16 +31,20 @@ const Missions = () => {
           </div>
         )}
         {!mission.reserved && (
-          <div className="status">
-            <button type="button" className="badge">
-              NOT A MEMBER
-            </button>
+          <div className="status w-64 flex justify-around text-xs">
+            <span
+              type="button"
+              className="badge bg-gray-500 text-white rounded-sm uppercase grid place-items-center px-1 py-0.5"
+            >
+              not a member
+            </span>
             <button
               type="button"
+              className="text-sm text-gray-600 border-gray-400 rounded-sm border-2 px-1 py-0.5"
               onClick={() => dispatch(joinMission(mission.mission_id))}
             >
               {' '}
-              join mission
+              Join Mission
             </button>
           </div>
         )}
@@ -45,12 +53,12 @@ const Missions = () => {
   ));
 
   return (
-    <table className="table-fixed">
+    <table className="table-auto">
       <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
+        <tr className="divide-x">
+          <th className="flex align-left pl-1">Mission</th>
+          <th className="ml-0">Description</th>
+          <th className="flex">Status</th>
         </tr>
       </thead>
       <tbody>{missionsDiv}</tbody>
